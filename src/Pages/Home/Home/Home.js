2b3401d065d89e9b6debed5e8508f1ea936eb729
gesora96 <nahidahmed9933@gmail.com>
2021-12-02 01:36:19 +0600

    subscribe area added

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navigation from '../../SharedComponents/Navigation/Navigation';
 import Banner from '../Banner/Banner';
+import Subscribe from '../Subscribe/Subscribe';
 
 const Home = () => {
   return (
     <>
       <Navigation />
       <Banner />
+      <Subscribe />
     </>
   );
 };
---
+++ src/Pages/Home/Subscribe/Subscribe.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './Subscribe.css';
+
+const Subscribe = () => {
+  const handleSubmit = (e) => e.preventDefault();
+
+  return (
+    <section id='subscribe'>
+      <div className='container'>
+        <h2>SUBSCRIBE FOR NEW FEATURES</h2>
+        <p>
+          We Provide some offers for our spacial customers regularly. If you
+          want to be a spacial customer please subscribe us. So that when we
+          provide offers we can notify you quickly.
+        </p>
+        <form className='email__form' onSubmit={handleSubmit}>
+          <input
+            type='email'
+            id='email'
+            autoComplete='off'
+            placeholder='Enter Your Email'
+            required
+          />
+          <button type='submit' className='submit__button'>
+            SUBSCRIBE
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Subscribe;
