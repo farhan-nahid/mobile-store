ad012c0fa875ed8fe70cf6d0eaa353ee484ec667
gesora96 <nahidahmed9933@gmail.com>
2021-12-03 21:42:48 +0600

    useProducts custom hook added

--- src/hooks/useProducts.js
+++ src/hooks/useProducts.js
@@ -10,7 +10,7 @@ const useProducts = () => {
       .then((res) => setProducts(res.data))
       .catch((err) => toast.error(err.message));
   }, []);
-  return [products, setProducts];
+  return [products];
 };
 
 export default useProducts;
