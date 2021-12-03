217d136810ab883d86b24bfd4cdbfa4459ac6118
gesora96 <nahidahmed9933@gmail.com>
2021-12-04 05:55:20 +0600

    profile added

---
+++ src/Pages/Dashboard/AddAdmin/AddAdmin.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './AddAdmin.css';
+
+const AddAdmin = () => {
+  return (
+    <section className='add__admin'>
+      <h3>Add New Admin</h3>
+    </section>
+  );
+};
+
+export default AddAdmin;
--- src/Pages/Dashboard/Profile/Profile.js
+++ src/Pages/Dashboard/Profile/Profile.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import useAuth from '../../../hooks/useAuth';
+import './Profile.css';
 
 const Profile = () => {
+  const {
+    loggedInUser: { photoURL, displayName, email },
+    logOut,
+  } = useAuth();
   return (
-    <div>
-      <h1>This is Profile</h1>
-    </div>
+    <section className='my__profile'>
+      <h3>My Profile</h3>
+      <div className='profile__container'>
+        <img src={photoURL} alt={displayName} />
+        <h5>{displayName}</h5>
+        <p>{email}</p>
+        <button className='brand__button' onClick={logOut}>
+          Log Out
+        </button>
+      </div>
+    </section>
   );
 };
 
