92eaf73415c1b5c56617688e662aaa3190a122bb
gesora96 <nahidahmed9933@gmail.com>
2021-12-04 03:09:53 +0600

    loading spinner fixed

--- src/Pages/SharedComponents/LoadingSpinner/LoadingSpinner.js
+++ src/Pages/SharedComponents/LoadingSpinner/LoadingSpinner.js
@@ -3,28 +3,26 @@ import './LoadingSpinner.css';
 
 const LoadingSpinner = () => {
   return (
-    <>
-      <section className='loading__spinner'>
-        <div class='loading__spinner__container'>
-          <div class='loading__spinner__content'>
+    <section className='loading__spinner'>
+      <div className='loading__spinner__container'>
+        <div className='loading__spinner__content'>
+          <div>
             <div>
-              <div>
-                <div></div>
-              </div>
-              <div>
-                <div></div>
-              </div>
-              <div>
-                <div></div>
-              </div>
-              <div>
-                <div></div>
-              </div>
+              <div></div>
+            </div>
+            <div>
+              <div></div>
+            </div>
+            <div>
+              <div></div>
+            </div>
+            <div>
+              <div></div>
             </div>
           </div>
         </div>
-      </section>
-    </>
+      </div>
+    </section>
   );
 };
 
